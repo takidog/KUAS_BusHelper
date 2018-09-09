3bc46a8e0ce25340331536e564b388b3c980bcbc
takidog <buluni.ha@gmail.com>
2018-09-09 22:42:00 +0800

    forget update Config.py :P

---
+++ Config.py
@@ -0,0 +1,18 @@
+import configparser
+class config:
+    def __init__(self,Config_file='Config.cfg'):
+        self.conf = configparser.ConfigParser()
+        self.conf.read(Config_file)
+
+    def get(self,sections,key):
+        if sections not in self.conf.sections():
+            return False
+        elif key not in self.conf.options(sections):
+            return False
+        return self.conf.get(sections,key)
+
+if __name__ == "__main__":
+    conf = config()
+    a = conf.get('KUAS_Default','account')
+    print(a)
+    print(type(a))
